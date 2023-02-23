236707987706e9bf8dbd898533271504aebd3f92
Aayush Dc <aayushdc123@yahoo.com>
2023-02-23 22:10:41 +0545

    Asynchronous Data Handling

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 let a =10;
 let b = 20;
 
-setTimeout(() => {
-    b = 50;
-}, 2000)
-console.log(a+b)
+
+
+let waitingData = new Promise((resolve, reject)=>{
+    setTimeout(() => {
+       resolve(50);
+    }, 2000)
+    
+})
+
+waitingData.then((data)=>{
+    b = data;
+    console.log(a+b)
+
+})
