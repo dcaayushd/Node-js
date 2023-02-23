f234fdf8defc781187c87f2d1dd196673536f6d9
Aayush Dc <aayushdc123@yahoo.com>
2023-02-23 21:52:27 +0545

    CRUD application in file

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const fs = require('fs');
-const path =  require('path')
-const dirPath = path.join(__dirname, 'files')
-// for(i=0; i<5; i++){
-//     fs.writeFileSync(`${dirPath}/apple${i}.txt`, 'This is an apple file')
-// }
+const path = require('path');
 
-fs.readdir(dirPath,(err, files)=>{
-    files.forEach((item)=>{
-        console.log(item)
-    })
-} )
+const dirPath = path.join(__dirname, 'crud')
+const filePath = `${dirPath}/apple.txt`
+// fs.writeFileSync(filePath,'This is a simple txt file')
+// fs.readFile(filePath,'utf8',(err, item)=>{
+//     console.log(item);
+// })
+// fs.appendFile(filePath, ' and file name is apple.txt', (err)=>{
+//     if(!err) console.log('File is updated Successfully')
+//     else console.log('Append failed')
+// })
+fs.rename(filePath, `${dirPath}/fruit.txt`, (err)=>{
+    if(!err) console.log('File is updated Successfully')
+    else console.log('File Renaming failed')
+})
+
+// fs.unlinkSync(`${dirPath}/fruit.txt`)
