ff2363f462c5a06292afaa3d1fef446dea0d9aba
Aayush Dc <aayushdc123@yahoo.com>
2023-02-24 17:17:36 +0545

    Express js Middleware

--- index.js
+++ index.js
@@ -1,47 +1,24 @@
 const express = require('express');
-const path = require('path');
-
 const app = express();
-const publicPath=path.join(__dirname,'public')
 
-
-app.set('view engine','ejs');
-
-app.get('',(_,resp)=>{
-    resp.sendFile(`${publicPath}/index.html`)
-});
-
-app.get('/profile',(_,resp)=>{
-    const user={
-        name:'Aayush',
-        email:'aayush@test.com',
-        country:'Nepal',
-        skills:[
-            'c',
-            'c++',
-            'java', 
-            'android', 
-            'dart', 
-            'node',
-        ]
+const reqFilter =(req,res,next)=>{
+    if(!req.query.age){
+        res.send('Please provide age')
     }
-   resp.render('profile',{user})
-});
+    else if(req.query.age <= 18){
+        res.send('You cannot access this page')
+    } else{
+        next()
+    }
+}
+app.use(reqFilter)
 
-app.get('/login', (_, res)=>{
-    res.render('login')
+
+app.get('/', (req, res) => {
+    res.send('Welcome to home page')
+})
+app.get('/users', (req, res) => {
+    res.send('Welcome to users page')
 })
 
-app.get('/contact',(_,resp)=>{
-    resp.sendFile(`${publicPath}/about.html`)
-});
-
-app.get('/help',(_,resp)=>{
-    resp.sendFile(`${publicPath}/help.html`)
-});
-
-app.get('*',(_,resp)=>{
-    resp.sendFile(`${publicPath}/404.html`)
-});
-
-app.listen(4000);
+app.listen(4000)
