9397f53243d4b4e230666bc18569fa9f876d79df
Aayush Dc <aayushdc123@yahoo.com>
2023-02-24 14:19:54 +0545

    Make HTML page

--- index.js
+++ index.js
@@ -1,32 +1,10 @@
 const express = require('express')
+const path = require('path')
+
 const app = express()
+const publicPath = path.join(__dirname,'/public')
 
-app.get('', (req, res) => {
-    res.send(`
-    <h1>Welcome, This is a home page</h1> 
-    <a href= '/about'>Go to about page</a>
-    `)
+app.use(express.static(publicPath))
 
-})
-app.get('/about', (req, res) => {
-    res.send(`
-        <input type ='text', placeholder = 'Username' value='${req.query.name}'/>
-        <button>
-        Click Me</button>
-        <a href= '/'>Go to home page</a>
-    `)
-})
-app.get('/help', (req, res) => {
-    res.send([
-        {
-            name: 'aayush',
-            email: 'aayush@test.com'
-        },
-        {
-            name: 'Sita',
-            email: 'sita@test.com'
-        },
-    ])
-})
 
 app.listen(4000)
