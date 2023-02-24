cbe61a3f55ff9b6f5dda2d458049513033aea8a0
Aayush Dc <aayushdc123@yahoo.com>
2023-02-24 14:41:31 +0545

    Remove extension from URL and 404 Page

--- index.js
+++ index.js
@@ -4,7 +4,20 @@ const path = require('path')
 const app = express()
 const publicPath = path.join(__dirname,'/public')
 
-app.use(express.static(publicPath))
+// app.use(express.static(publicPath))
+
+app.get('',(_,res)=>{
+    res.sendFile(`${publicPath}/index.html`)
+})
+app.get('/about',(_,res)=>{
+    res.sendFile(`${publicPath}/about.html`)
+})
+app.get('/help',(_,res)=>{
+    res.sendFile(`${publicPath}/help.html`)
+})
+app.get('*',(_,res)=>{
+    res.sendFile(`${publicPath}/404.html`)
+})
 
 
 app.listen(4000)
