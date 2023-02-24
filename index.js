054770e6ce6c985cd6a96514847d8bfc5ba1816f
Aayush Dc <aayushdc123@yahoo.com>
2023-02-24 13:57:18 +0545

    Render HTML and JSON

--- index.js
+++ index.js
@@ -1,16 +1,32 @@
 const express = require('express')
 const app = express()
 
-app.get('',(req, res)=>{
-    res.send('Welcome User, This is a home page')
+app.get('', (req, res) => {
+    res.send(`
+    <h1>Welcome, This is a home page</h1> 
+    <a href= '/about'>Go to about page</a>
+    `)
+
 })
-app.get('/about',(req, res)=>{
-    res.send('Welcome User, This is a About page')
+app.get('/about', (req, res) => {
+    res.send(`
+        <input type ='text', placeholder = 'Username' value='${req.query.name}'/>
+        <button>
+        Click Me</button>
+        <a href= '/'>Go to home page</a>
+    `)
 })
-app.get('/help',(req, res)=>{
-    res.send('Welcome User, This is a Help page')
+app.get('/help', (req, res) => {
+    res.send([
+        {
+            name: 'aayush',
+            email: 'aayush@test.com'
+        },
+        {
+            name: 'Sita',
+            email: 'sita@test.com'
+        },
+    ])
 })
-app.get('/contact',(req, res)=>{
-    res.send('Welcome User, This is a Contact page')
-})
+
 app.listen(4000)
