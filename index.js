05c1d1fb9a54a4d54f109909d6450f6812a61e82
Aayush Dc <aayushdc123@yahoo.com>
2023-02-26 22:02:47 +0545

    GET, DELETE and PUT api method with mongoose

--- index.js
+++ index.js
@@ -8,7 +8,34 @@ app.post('/create', async (req, res) => {
     let data = new product(req.body)
     let result = await data.save()
     res.send(result)
-    console.log(result)
+})
+
+app.get('/list', async (req, res)=>{
+    let data = await product.find()
+    res.send(data)
+})
+
+app.delete('/delete/:_id', async (req, res)=>{
+    console.log(req.params)
+    let data = await product.deleteOne(req.params)
+    res.send(data)
+})
+
+app.put('/update/:_id', async (req, res)=>{
+    console.log(req.params)
+    let data = await product.updateOne(
+        ////We are just using the id if we had sent name then these curly braces would be compulsory
+        // {
+            // name:'m40'
+            req.params
+        // }
+        , 
+    {$set:
+        // {
+        req.body
+    // }
+})
+    res.send(data)
 })
 
 app.listen(4000)
