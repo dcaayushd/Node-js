2fbb51ae4c236a151090778e9b96afec8c4e59b4
Aayush Dc <aayushdc123@yahoo.com>
2023-02-26 20:48:24 +0545

    mongoose with node

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
 const mongoose = require('mongoose')
-
-const main =async() =>{
-    await mongoose.connect('mongodb://localhost:27017/e-comm')
-    const productSchema = new mongoose.Schema({
-        name: String,
-        price: Number,
-    })
+mongoose.connect('mongodb://localhost:27017/e-comm')
+const productSchema = new mongoose.Schema({
+  name: String,
+  price: Number,
+  brand: String,
+	category: String,
+})
+const saveInDB = async () => {
     const productModel = mongoose.model('products', productSchema)
-    let data = new productModel({name: 'A30', price:130})
+    let data = new productModel(
+        {
+            name: '12 S Ultra',
+            price: 1299,
+            brand: 'Xiaomi',
+            category: 'mobile',
+        }
+    )
     let result = await data.save()
     console.log(result)
 }
 
-main()
+saveInDB()
