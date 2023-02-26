999e9d7d67a0507b7d04ab9fa84330222ddba8da
Aayush Dc <aayushdc123@yahoo.com>
2023-02-26 12:35:38 +0545

    PUT API | Update data in MongoDB

--- api.js
+++ api.js
@@ -10,10 +10,21 @@ app.get('/', async (req, res) => {
     res.send(data)
 })
 
-app.post('/', async (req, res)=>{
+app.post('/', async (req, res) => {
     let data = await dbConnect()
     let result = await data.insertOne(req.body)
     res.send(result)
+})
+
+app.put('/:name', async (req, res) => {
+    let data = await dbConnect()
+    let result = await data.updateOne(
+        {name: req.params.name}, 
+        {$set: req.body}
+    )
+    // console.log(req.body)
+    res.send({ result: 'update' })
+
 })
 
 app.listen(4000)
