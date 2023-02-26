86040855f70031c35df9d5fcd08e0d285fb2c05c
Aayush Dc <aayushdc123@yahoo.com>
2023-02-26 11:43:05 +0545

    Post API | insert data in MongoDB

--- api.js
+++ api.js
@@ -2,9 +2,18 @@ const express = require('express')
 const dbConnect = require('./mongodb')
 const app = express()
 
+app.use(express.json())
+
 app.get('/', async (req, res) => {
     let data = await dbConnect()
     data = await data.find().toArray()
     res.send(data)
 })
+
+app.post('/', async (req, res)=>{
+    let data = await dbConnect()
+    let result = await data.insertOne(req.body)
+    res.send(result)
+})
+
 app.listen(4000)
