3becb9468289d84cb289555a0615434fb8186e71
Aayush Dc <aayushdc123@yahoo.com>
2023-02-27 00:54:20 +0545

    Search API with multiple filed

--- index.js
+++ index.js
@@ -4,37 +4,22 @@ const product = require('./product')
 
 const app = express()
 app.use(express.json())
-app.post('/create', async (req, res) => {
-    let data = new product(req.body)
-    let result = await data.save()
-    res.send(result)
-})
 
-app.get('/list', async (req, res)=>{
-    let data = await product.find()
-    res.send(data)
-})
-
-app.delete('/delete/:_id', async (req, res)=>{
-    console.log(req.params)
-    let data = await product.deleteOne(req.params)
-    res.send(data)
-})
-
-app.put('/update/:_id', async (req, res)=>{
-    console.log(req.params)
-    let data = await product.updateOne(
-        ////We are just using the id if we had sent name then these curly braces would be compulsory
-        // {
-            // name:'m40'
-            req.params
-        // }
-        , 
-    {$set:
-        // {
-        req.body
-    // }
-})
+app.get('/search/:key', async (req, res)=>{
+    let data = await product.find(
+        {
+            '$or':[
+                {
+                    "name":{
+                        $regex: req.params.key
+                    },
+                    "brand":{
+                        $regex: req.params.key
+                    }
+                }
+            ]
+        }
+    )
     res.send(data)
 })
 
