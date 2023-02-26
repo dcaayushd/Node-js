5e54a688df47b7b0b29b4f780c71b65b9d619469
Aayush Dc <aayushdc123@yahoo.com>
2023-02-26 21:37:17 +0545

    Post API with mongoose ,Make post API in Node js

--- index.js
+++ index.js
@@ -1,77 +1,14 @@
-const mongoose = require('mongoose')
-mongoose.connect('mongodb://localhost:27017/e-comm')
+const express = require('express');
+require('./config')
+const product = require('./product')
 
-////    Basic Example
-// const productSchema = new mongoose.Schema({
-//     name: String,
-//     price: Number,
-// })
-// const main = async () => {
-
-//     const productModel = mongoose.model('products', productSchema)
-//     let data = new productModel({ name: 'A30', price: 130 })
-//     let result = await data.save()
-//     console.log(result)
-// }
-// main()
-
-////    Save in DB
-// const productSchema = new mongoose.Schema({
-//     name: String,
-//     price: Number,
-//     brand: String,
-//     category: String,
-// })
-// const saveInDB = async () => {
-//     const productModel = mongoose.model('products', productSchema)
-//     let data = new productModel(
-//         {
-//             name: '12 S Ultra',
-//             price: 1299,
-//             brand: 'Xiaomi',
-//             category: 'mobile',
-//         }
-//     )
-//     let result = await data.save()
-//     console.log(result)
-// }
-// saveInDB()
-
-////    Update in DB
-const productSchema = new mongoose.Schema({
-    name: String,
-    price: Number,
-    brand: String,
-    category: String,
+const app = express()
+app.use(express.json())
+app.post('/create', async (req, res) => {
+    let data = new product(req.body)
+    let result = await data.save()
+    res.send(result)
+    console.log(result)
 })
-// const updateInDB = async () => {
-//     const productModel = mongoose.model('products', productSchema)
-//     let data = await productModel.updateOne(
-//         { name: '12 S Ultra' }, 
-//         {
-//             $set: { price: 1399 } 
-//         }
-//     )
-//     console.log(data)
-// }
 
-// updateInDB()
-
-// const deleteInDB = async () => {
-//     const productModel = mongoose.model('products', productSchema)
-//     let data = await productModel.deleteOne({name:'A30'})
-//     console.log(data)
-// }
-
-// deleteInDB()
-
-
-////    Find in DB
-const findInDB = async () => {
-    const productModel = mongoose.model('products', productSchema)
-    let data = await productModel.find({})
-    console.log(data)
-}
-
-findInDB()
-
+app.listen(4000)
