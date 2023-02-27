c2d7ea931d9655dda87732aa772338f8a7554a54
Aayush Dc <aayushdc123@yahoo.com>
2023-02-27 22:00:18 +0545

    DELETE API with MySQL

--- index.js
+++ index.js
@@ -6,31 +6,37 @@ app.use(express.json());
 
 app.get("/", (req, resp) => {
     con.query("select * from users", (err, result) => {
-        if (err) { 
-            resp.send("error in api") 
+        if (err) {
+            resp.send("error in api")
         }
-        else { 
-            resp.send(result) 
+        else {
+            resp.send(result)
         }
     })
 });
 
-app.post("/",(req,resp)=>{
-    const data=req.body;
-    con.query("INSERT INTO users SET?",data,(error,results,fields)=>{
-      if(error) throw error;
-      resp.send(results)
+app.post("/", (req, resp) => {
+    const data = req.body;
+    con.query("INSERT INTO users SET?", data, (error, results, fields) => {
+        if (error) throw error;
+        resp.send(results)
     })
-  });
+});
 
-  app.put("/:id",(req,resp)=>{
-    const data= [req.body.name,req.body.password,req.body.user_type,req.params.id];
+app.put("/:id", (req, resp) => {
+    const data = [req.body.name, req.body.password, req.body.user_type, req.params.id];
     con.query("UPDATE users SET name = ?, password = ?, user_type = ? WHERE id = ?",
-    data,(error,results,fields)=>{
-      if(error) throw error;
-      resp.send(results)
-    })
-  })
+        data, (error, results, fields) => {
+            if (error) throw error;
+            resp.send(results)
+        })
+})
 
+app.delete("/:id", (req, res) => {
+    con.query("DELETE FROM users  WHERE id = ?" + req.params.id, (error, results) => {
+        if (error) throw error;
+        res.send(results)
+    })
+})
 
 app.listen("4000")
